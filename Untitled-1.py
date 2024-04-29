4f5c68c0b8d1c4c38bc6198ea0870ab86a520ab5
marcoferjim <151623197+marcoferjim@users.noreply.github.com>
2024-04-29 11:18:58 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,23 +1,50 @@
-#empezamos con la edición del código
-# Importar las bibliotecas necesarias
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tools.eval_measures import rmse
 
-# Cargar los datos de la serie temporal (por ejemplo, datos de ventas mensuales)
-data = pd.read_csv("C:/Users/esthe/Downloads/CEU/1ºCARRERA/Segundo semestre/Proyecto 1/Código ARIMA/datos_pasajeros.csv")
+data = pd.read_csv("/Users/marcofernandez/Desktop/grupo metro/datos_pasajeros.csv")
 data = data.dropna()
-pd.to_numeric(data)
 
 data
-# Visualizar los primeros registros de los datos
 print(data.head())
 
-# Graficar la serie temporal
 data.plot()
-plt.xlabel('Número de psajeros')
-plt.ylabel('Valor')
+plt.xlabel('num pas')
+plt.ylabel('valor')
 plt.title('Serie Temporal')
 plt.show()
+
+
+
+train_size = int(len((data)) * 0.8)
+train, test = data[:train_size], data[train_size:]
+train
+test
+test.tail(10)
+order = (5, 1, 0)  
+model = ARIMA(data, order=order)
+model_fit = model.fit()
+model_fit.forecast(steps=3)
+
+print(model_fit.summary())
+
+test
+predictions = model_fit.forecast(steps=len(test))
+print(predictions)
+
+rmse_value = rmse(test, predictions)
+print('Error cuadrático medio (RMSE):', rmse_value)
+
+plt.plot(test.tail(10).index, test.tail(10), label='Observado')
+ploteado=plt.plot(test.tail(10).index, predictions[0:10], color='red', label='Predicción')
+plt.xlabel('num pas')
+plt.ylabel('Valor')
+plt.title('Predicciones ARIMA')
+plt.legend()
+plt.show()
+
+test.index
+
+predictions
